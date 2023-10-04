a3fe2ebaacfc55bf4d854c065da933416aa24412
RihoMiyamoto <rmiyamoto7890@gmail.com>
2023-10-04 13:45:06 -0500

    added the message

--- GradeCalc/main.js
+++ GradeCalc/main.js
@@ -1,6 +1,6 @@
+var average;
 function mult(value) {
     var o, t, h, f, i, s;
-    var average;
     o = document.getElementById("class1").value / 6;
     t = document.getElementById("class2").value / 6;
     h = document.getElementById("class3").value / 6;
@@ -15,25 +15,37 @@ function mult(value) {
 }
 
 function changeColor(obj) {
-    if (obj>=90){
-        document.getElementById("grade_point_scale").style.backgroundColor ='rgb(40, 252, 3)';
-        document.getElementById("colorChange").style.backgroundColor= 'rgb(151, 255, 177)';
-        document.getElementById("forms").style.color= 'black';
-    }else if (obj>=80 && obj <90){
+    var amtLeft =0;
+    var best = "Congrats on the " + obj;
+    var mid = "Not bad";
+    if (obj >= 90) {
+        document.getElementById("grade_point_scale").style.backgroundColor = 'rgb(40, 252, 3)';
+        document.getElementById("colorChange").style.backgroundColor = 'rgb(151, 255, 177)';
+        document.getElementById("forms").style.color = 'black';
+        document.getElementById("message").innerHTML = best;
+    } else if (obj >= 80 && obj < 90) {
         document.getElementById("grade_point_scale").style.backgroundColor = 'rgb(184, 255, 54)';
-        document.getElementById("colorChange").style.backgroundColor= 'rgb(203, 255, 158)';
-        document.getElementById("forms").style.color= 'black';
-    }else if (obj>=70 && obj <80){
+        document.getElementById("colorChange").style.backgroundColor = 'rgb(203, 255, 158)';
+        document.getElementById("forms").style.color = 'black';
+        document.getElementById("message").innerHTML = mid;
+    } else if (obj >= 70 && obj < 80) {
         document.getElementById("grade_point_scale").style.backgroundColor = 'rgb(255, 225, 0)';
-        document.getElementById("colorChange").style.backgroundColor= 'rgb(255, 249, 158)';
-        document.getElementById("forms").style.color= 'black';
-    }else if (obj>=60 && obj <70){
+        document.getElementById("colorChange").style.backgroundColor = 'rgb(255, 249, 158)';
+        document.getElementById("forms").style.color = 'black';
+        document.getElementById("message").innerHTML = mid;
+    } else if (obj >= 60 && obj < 70) {
         document.getElementById("grade_point_scale").style.backgroundColor = 'rgb(255, 136, 0)';
-        document.getElementById("colorChange").style.backgroundColor= 'rgb(255, 167, 105)';
-        document.getElementById("forms").style.color= 'black';
-    }else if (obj <60){
+        document.getElementById("colorChange").style.backgroundColor = 'rgb(255, 167, 105)';
+        document.getElementById("forms").style.color = 'black';
+        amtLeft = 70 - obj;
+        worst = "You have " + amtLeft + " points left for a 70";
+        document.getElementById("message").innerHTML = worst;
+    } else if (obj < 60) {
         document.getElementById("grade_point_scale").style.backgroundColor = 'rgb(255, 0, 0)';
-        document.getElementById("colorChange").style.backgroundColor= 'rgb(255, 161, 153)';
-        document.getElementById("forms").style.color= 'white';
+        document.getElementById("colorChange").style.backgroundColor = 'rgb(255, 161, 153)';
+        document.getElementById("forms").style.color = 'white';
+        amtLeft = 70 - obj;
+        worst = "You have " + amtLeft + " points left for a 70";
+        document.getElementById("message").innerHTML = worst;
     }
 }
